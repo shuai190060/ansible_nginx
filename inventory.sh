c77103cdbd555461deb886e5c75bdd8b9fb5969b
Shuhai Liao <shuai190060roy@gmail.com>
2023-07-13 14:11:52 +0200

    update the inventory script

--- inventory.sh
+++ inventory.sh
@@ -5,6 +5,7 @@ echo "" > inventory
 # Extract values from JSON output
 bastion_ip=$(terraform output -json | jq -r '.bastion_ip.value')
 vm1_ip=$(terraform output -json | jq -r '.vm1_ip.value')
+node_ip=$(terraform output -json | jq -r '.node_ip.value')
 
 # inventory for bastion host
 bastion="bastion_host ansible_host={bastion_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/ansible"
@@ -23,6 +24,21 @@ vm1=$(echo "$vm1" | sed "s/{bastion_ip}/$bastion_ip/g")
 vm1=$(echo "$vm1" | sed "s/{vm1_ip}/$vm1_ip/g")
 echo "[private]" >> inventory
 echo $vm1 >> inventory
+
+# create a k8s group for k8s ec2
+echo "[k8s]" >> inventory
+master="master ansible_host={bastion_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/ansible"
+master=$(echo "$master" | sed "s/{bastion_ip}/$bastion_ip/g")
+echo $master >> inventory
+node="node ansible_host={node_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/ansible"
+node=$(echo "$node" | sed "s/{node_ip}/$node_ip/g")
+echo $node >> inventory
+
+echo "[master]" >> inventory
+echo $master >> inventory
+
+echo "[worker]" >> inventory
+echo $node >> inventory
 
 
 
